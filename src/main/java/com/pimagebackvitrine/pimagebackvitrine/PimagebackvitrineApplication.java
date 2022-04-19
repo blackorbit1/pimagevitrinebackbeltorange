0c2e1e52e90760db82720cb0cb4873902c9a95f1
Enzo Dutra <38140797+blackorbit1@users.noreply.github.com>
2022-04-19 11:19:24 +0200

    initial commit

---
+++ src/main/java/com/pimagebackvitrine/pimagebackvitrine/PimagebackvitrineApplication.java
@@ -0,0 +1,13 @@
+package com.pimagebackvitrine.pimagebackvitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimagebackvitrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PimagebackvitrineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pimagebackvitrine/pimagebackvitrine/PimagebackvitrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimagebackvitrine.pimagebackvitrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimagebackvitrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
